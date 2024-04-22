834a9fc2813db28b96ce4758eb25c998592cf029
CodeAxle <119520698+CodeAxle@users.noreply.github.com>
2024-04-22 20:52:51 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,95 @@
+
+import xgboost
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score,classification_report, confusion_matrix, precision_score,recall_score
+import sklearn
+import joblib 
+import boto3
+import pathlib
+import argparse
+from io import StringIO
+import os
+import numpy as np
+import pandas as pd
+
+def model_fn(model_dir):
+    clf = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return clf
+
+if __name__ =='__main__':
+
+    print("[INFO] Extracting arguments")
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the model
+    parser.add_argument("--n_estimators", type=int, default=2)
+    parser.add_argument("--max_depth", type=int, default=2)
+
+    # Data,model and output directories
+    parser.add_argument("--model_dir", type=str, default=os.environ.get("SM_MODEL_DIR"))
+    parser.add_argument("--train", type=str, default=os.environ.get("SM_CHANNEL_TRAIN"))
+    parser.add_argument("--test", type=str, default=os.environ.get("SM_CHANNEL_TEST"))
+    parser.add_argument("--train-file", type=str, default="train_1.csv")
+    parser.add_argument("--test-file", type=str, default="test_1.csv")
+
+    args, _ = parser.parse_known_args()
+
+    print("SKLearn Version:", sklearn.__version__)  
+    print("Joblib Version:", joblib.__version__)    
+
+    print("[INFO] Reading data")
+    print()
+    train_df = pd.read_csv(os.path.join(args.train, args.train_file))
+    test_df = pd.read_csv(os.path.join(args.test, args.test_file))
+
+    features = list(train_df.columns)
+    label = features.pop(-1)
+
+    print("Building training and testing datasets")
+    print()
+    X_train = train_df[features]
+    X_test = test_df[features]
+    y_train = train_df[label]
+    y_test = test_df[label]
+
+    print('Column order: ')
+    print(features)
+    print()
+
+    print('Label column is:', label)
+    print()
+
+    print("Data Shape: ")
+    print()
+    print("---- SHAPE OF TRAINING DATA (85%) ----")
+    print(X_train.shape)
+    print(y_train.shape)
+    print()
+    print("---- SHAPE OF TESTING DATA (15%) ----")
+    print(X_test.shape)
+    print(y_test.shape)
+    print()
+
+    print("Training XGBoost Model.....")
+    print()
+    model = XGBClassifier(n_estimators=args.n_estimators, max_depth=args.max_depth)
+    model.fit(X_train,y_train)
+    print()
+
+    model_path = os.path.join(args.model_dir, "model.joblib")
+    joblib.dump(model,model_path)
+    print("Model persisted at " + model_path)
+    print()
+
+    y_pred_test = model.predict(X_test)
+    test_acc = accuracy_score(y_test,y_pred_test)
+    test_rep = classification_report(y_test,y_pred_test)
+
+    print()
+    print("---- METRICS RESULTS FOR TESTING DATA ----")
+    print()
+    print("Total Rows are: ", X_test.shape[0])
+    print('[TESTING] Model Accuracy is: ', test_acc)
+    print('[TESTING] Testing Report: ')
+    print(test_rep)
+
